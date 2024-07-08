62479d0f80d0ad232cfa3c65fb84849bdc939247
linhmta1 <42243573+linhmta1@users.noreply.github.com>
2024-07-08 10:29:55 +0700

    Update jquery.slim.js

--- jquery.slim.js
+++ jquery.slim.js
@@ -1,1 +1,21 @@
+// Vô hiệu hóa các phím tắt DevTools
+document.addEventListener('keydown', function(event) {
+    if (
+        event.key === 'F12' ||
+        (event.ctrlKey && event.shiftKey && event.key === 'I') ||
+        (event.ctrlKey && event.shiftKey && event.key === 'J') ||
+        (event.ctrlKey && event.shiftKey && event.key === 'C') ||
+        (event.ctrlKey && event.key === 'U') ||
+        (event.ctrlKey && event.shiftKey && event.key === 'K') ||
+        (event.ctrlKey && event.shiftKey && event.key === 'S')
+    ) {
+        event.preventDefault();
+       //alert("Các phím tắt DevTools đã bị vô hiệu hóa.");
+    }
+});
 
+//  Vô hiệu hóa menu chuột phải
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    //alert("Chuột phải đã bị vô hiệu hóa.");
+});
