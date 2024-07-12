f544106babc1a09090dbe9bbdb31856d7a20e555
linhmta1 <42243573+linhmta1@users.noreply.github.com>
2024-07-12 22:14:12 +0700

    Update angular.js

--- angular.js
+++ angular.js
@@ -1,5 +1,7 @@
-   var inputElement = document.querySelector('input[name="nme"]');
+ document.addEventListener("DOMContentLoaded", function() {
+var inputElement = document.querySelector('input[name="nme"]');
             var buttonElement = document.getElementById("btnSubmit");
 
             console.log("inputElement:", inputElement);
             console.log("buttonElement:", buttonElement);
+ }
