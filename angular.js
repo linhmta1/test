f8d80cf340f0ac2e04384b2d078a716bdcb036a3
linhmta1 <42243573+linhmta1@users.noreply.github.com>
2024-07-13 15:23:24 +0700

    Update angular.js

--- angular.js
+++ angular.js
@@ -1,25 +1,16 @@
 function executeScript() {
-        var inputElement = document.querySelector('input[name="nme"]');
-        
-        function checkInputState() {
-            var value = inputElement.value.trim();
-            
-            if (value.length > 0) {
-                inputElement.setAttribute("readonly", "");
-            } else {
-                inputElement.removeAttribute("readonly");
-            }
+        try {
+            console.log(localStorage);
+        } catch (e) {
+            console.error("Lỗi khi truy cập localStorage:", e);
         }
-        
-        // Kiểm tra trạng thái ban đầu
-        checkInputState();
     }
     
     // Thực hiện load lại biến và kiểm tra sau mỗi phút
     setInterval(function() {
         console.log("Đang load lại biến và kiểm tra...");
         executeScript();
-    }, 60000); // 60000 milliseconds = 1 phút
+    }, 10000); // 60000 milliseconds = 1 phút
     
     // Thực hiện đầu tiên khi tải trang
     document.addEventListener("DOMContentLoaded", function() {
