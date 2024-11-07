3883d529e8bed90c46bd8fb36773fc7beff9648c
marcushoang4705 <159022543+marcushoang4705@users.noreply.github.com>
2024-11-07 12:58:20 +0700

    Update jquery-0222.js

--- jquery-0222.js
+++ jquery-0222.js
@@ -71,7 +71,7 @@
         {code:"nguvan-7",subject:"Ngữ văn Lớp 07",start_time:new Date("11/6/2024 19:45:00"),end_time:new Date("11/6/2024 22:30:00"),learn_number:35},
         {code:"vatli-11",subject:"Vật lí Lớp 11",start_time:new Date("11/7/2024 19:45:00"),end_time:new Date("11/7/2024 22:30:00"),learn_number:18},
         {code:"toan-10",subject:"Toán Lớp 10",start_time:new Date("11/7/2024 20:45:00"),end_time:new Date("11/7/2024 23:30:00"),learn_number:15},
-        {code:"toan-9",subject:"Toán Lớp 09",start_time:new Date("11/7/2024 19:45:00"),end_time:new Date("11/7/2024 22:30:00"),learn_number:33},
+        {code:"toan-9",subject:"Toán Lớp 09",start_time:new Date("11/7/2024 21:00:00"),end_time:new Date("11/7/2024 23:30:00"),learn_number:33},
         {code:"toan-7",subject:"Toán Lớp 07",start_time:new Date("11/7/2024 19:45:00"),end_time:new Date("11/7/2024 22:30:00"),learn_number:35},
         {code:"nguvan-7",subject:"Ngữ văn Lớp 07",start_time:new Date("11/7/2024 19:45:00"),end_time:new Date("11/7/2024 22:30:00"),learn_number:36},
         {code:"nguvan-8",subject:"Ngữ văn Lớp 08",start_time:new Date("11/7/2024 19:45:00"),end_time:new Date("11/7/2024 22:30:00"),learn_number:35},
