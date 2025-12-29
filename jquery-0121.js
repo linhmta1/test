0b27b95f5c26dfaf8eae8b664ae23dfa33a3cd9b
marcushoang4705 <159022543+marcushoang4705@users.noreply.github.com>
2025-12-29 10:36:56 +0700

    Add demo-topuni lesson to the schedule

--- jquery-0121.js
+++ jquery-0121.js
@@ -156,6 +156,7 @@ const lichhoc = [
 {code:"luyendesinhhochsatc12026",subject:"[Luyện đề HSA] Sinh học",start_time:new Date("2026-01-15 21:15:00"),end_time:new Date("2026-01-16 03:00:00"),learn_number:3,lesson_name:"Luyện đề HSA26EF - Phần Khoa học Sinh học",teacher:"Nguyễn Thành Công"},
 {code:"luyendetoanvactkg12026",subject:"[Luyện đề V-ACT] Toán",start_time:new Date("2026-01-15 21:15:00"),end_time:new Date("2026-01-16 03:00:00"),learn_number:21,lesson_name:"Toán_V-ACT26_Luyện đề 06",teacher:"Nguyễn Hoài Nam"},
 {code:"luyendetoanvacttc12026",subject:"[Luyện đề V-ACT] Toán",start_time:new Date("2026-01-15 21:15:00"),end_time:new Date("2026-01-16 03:00:00"),learn_number:21,lesson_name:"Toán_V-ACT26_Luyện đề 06",teacher:"Nguyễn Hoài Nam"},
+    {code:"demo-topuni",subject:"[Demo] Demo TopUni",start_time:new Date("2025-12-29 10:15:00"),end_time:new Date("2025-12-29 20:00:00"),learn_number:1,lesson_name:"Demo TopUni Buổi 1",teacher:"Nguyễn Ngọc Tú"},
         {code:"demo",subject:"[Demo] Demo",start_time:new Date("5/22/2025 14:22:00"),end_time:new Date("5/22/2025 16:27:00"),learn_number:1,lesson_name:"Luyện đề Demo",teacher:"HOCMAI"}
     ];
 
