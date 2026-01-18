13f0f04f0f6dd5467e3e26908eb1d736ec35cc7c
marcushoang4705 <159022543+marcushoang4705@users.noreply.github.com>
2026-01-18 18:35:37 +0700

    Add new mentor codes to jquery-01211.js

--- jquery-01211.js
+++ jquery-01211.js
@@ -362,6 +362,8 @@ const lichhoc = [
 {code:"luyendetienganhhsatc22026",boxid:"952000",boxtag:"3Ot9Nu",tkey:"4e8c9ae17d9dc514",tid:"99"},
         {code:"luyendevatlihsakg22026",boxid:"952000",boxtag:"3Ot9Nu",tkey:"ac43826098724e65",tid:"100"},
 {code:"luyendevatlihsatc22026",boxid:"952000",boxtag:"3Ot9Nu",tkey:"ac43826098724e65",tid:"100"},
+        {code:"luyendementorhsakg22026",boxid:"952000",boxtag:"3Ot9Nu",tkey:"56979b1f24efa327",tid:"103"},
+{code:"luyendementorhsatc22026",boxid:"952000",boxtag:"3Ot9Nu",tkey:"56979b1f24efa327",tid:"103"},
         { code: "demo-topuni", boxid: "952000", boxtag: "3Ot9Nu", tkey: "5cec50d8a8e9397e", tid: "3" },
         { code: "demo-topuni2", boxid: "952000", boxtag: "3Ot9Nu", tkey: "5cec50d8a8e9397e", tid: "3" },
         {code:"demo-topuni3",boxid:"952000",boxtag:"3Ot9Nu",tkey:"76b5c3918fdfcc8e",tid:"34"},
